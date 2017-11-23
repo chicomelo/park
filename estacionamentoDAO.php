f5db26d55aab9e3d68184984da1bc7f452e3bab6
Joao Francisco <jfpmelo@gmail.com>
2017-11-23 20:35:28 -0200

    up

--- estacionamentoDAO.php
+++ estacionamentoDAO.php
@@ -45,19 +45,55 @@ class CarrosDAO extends Sql {
         array(":PLACA"=>$placa, ":MODELO"=>$modelo, ":COR"=>$cor, ":COD_CARRO"=>$cod_carro));
         return ($resultado);
     }
-    function deletar();
+    function deletar($cod_carro){
+        $sql = new Sql();
+
+        $resultado = $sql -> query("DELETE FROM carros WHERE cod_carro = :COD_CARRO",
+        array(":COD_CARRO"=>$cod_carro));
+        return ($resultado);
+    }
 }
 
 class VagasDAO extends Sql{
-    function inserir();
-    function atualizar();
-    function deletar();
+    function atualizar($numero){
+        $sql = new Sql();
+
+        $resultado = $sql -> query("UPDATE vagas
+                                        SET ativo = :ATIVO,
+                                        modelo = :MODELO,
+                                        cor = :COR
+                                        WHERE numero = :NUMERO",
+        array(":ATIVO"=>$ativo, ":COD_VAGA"=>$numero));
+        return ($resultado);
+    }
 }
 
 class TicketsDAO extends Sql{
-    function inserir();
-    function atualizar();
-    function deletar();
+    function inserir(){
+        $sql = new Sql();
+
+        $resultado = $sql -> query("INSERT INTO tickets (cod_vaga, cod_carro, data_chegada)
+                                        VALUES (:COD_VAGA, :COD_CARRO, :DATA_CHEGADA)",
+        array(":COD_VAGA"=>$cod_vaga, ":COD_CARRO"=>$cod_carro ,":DATA_CHEGADA"=>$data_chegada));
+        return ($resultado);
+    }
+    function atualizar($data_saida, $valor, $cod_ticket){
+        $sql = new Sql();
+
+        $resultado = $sql -> query("UPDATE tickets
+                                        SET data_saida = :DATA_SAIDA,
+                                        valor = :VALOR
+                                        WHERE cod_ticket = :COD_TICKET",
+        array(":DATA_SAIDA"=>$data_saida, ":VALOR"=>$valor, ":COD_TICKET"=>$cod_ticket));
+        return ($resultado);
+    }
+    function deletar($cod_ticket){
+        $sql = new Sql();
+
+        $resultado = $sql -> query("DELETE FROM tickets WHERE cod_ticket = :COD_TICKET",
+        array(":COD_TICKET"=>$cod_ticket));
+        return ($resultado);
+    }
     function buscarInfoCarro($placa){
 
         $sql = new Sql();
--- index.php
+++ index.php
@@ -34,26 +34,27 @@
 
 
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "vertrigo";
-$dbname = "estacionamento";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+//
+// $servername = "localhost";
+// $username = "root";
+// $password = "vertrigo";
+// $dbname = "estacionamento";
+//
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// // Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 //
 // for($i = 0; $i<80; $i++){
-// $sql = "INSERT INTO vagas (status) VALUES ('0')";
+// $sql = "INSERT INTO vagas (numero, ativo) VALUES ($i+1, '1')";
 // if ($conn->query($sql) === TRUE) {
 //     echo "New record created successfully";
 // } else {
 //     echo "Error: " . $sql . "<br>" . $conn->error;
 // }
 // }
-
-$conn->close();
+//
+// $conn->close();
 ?>
