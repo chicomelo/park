0c8dad1547dd4a332d25d1f73e5e7ae2913f0958
Joao Francisco <jfpmelo@gmail.com>
2017-11-23 19:31:17 -0200

    construção da DAO

--- estacionamentoDAO.php
+++ estacionamentoDAO.php
@@ -26,8 +26,25 @@
 require_once ("sql.php");
 
 class CarrosDAO extends Sql {
-    function inserir();
-    function atualizar();
+    function inserir($placa, $modelo, $cor){
+        $sql = new Sql();
+
+		$resultado = $sql -> query("INSERT INTO carros (placa, modelo, cor)
+										VALUES (:PLACA, :MODELO, :COR)",
+		array(":PLACA"=>$placa, ":MODELO"=>$modelo ,":COR"=>$cor));
+		return ($resultado);
+    }
+    function atualizar($cod_carro, $placa, $modelo, $cor){
+        $sql = new Sql();
+
+        $resultado = $sql -> query("UPDATE carros
+                                        SET placa = :PLACA,
+                                        modelo = :MODELO,
+                                        cor = :COR
+                                        WHERE cod_carro = :COD_CARRO",
+        array(":PLACA"=>$placa, ":MODELO"=>$modelo, ":COR"=>$cor, ":COD_CARRO"=>$cod_carro));
+        return ($resultado);
+    }
     function deletar();
 }
 
@@ -41,7 +58,15 @@ class TicketsDAO extends Sql{
     function inserir();
     function atualizar();
     function deletar();
-    function buscarInfoCarro($placa,$modelo=null){
+    function buscarInfoCarro($placa){
+
+        $sql = new Sql();
+
+        $query = "SELECT * FROM carros WHERE placa = $placa ";
+
+		$results = $sql->select("SELECT * FROM carros WHERE placa = :PLACA", array(":PLACA"=>$placa));
+		return $results;
+
         # array com as informações do carro
         /*
         id carro, modelo carro
