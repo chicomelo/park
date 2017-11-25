a00e0439840b5d6a8f0698259352404287752860
Joao Francisco <jfpmelo@gmail.com>
2017-11-25 01:42:02 -0200

    up

---
+++ estacionamento.php
@@ -0,0 +1,8 @@
+<?php
+header('Cache-Control: no-cache, must-revalidate');
+header('Content-Type: text/html; charset=utf-8');
+
+
+print_r($_POST);
+
+?>
--- index.php
+++ index.php
@@ -27,28 +27,15 @@
                             <p class="bem-vindo">Seja bem-vindo ao Panda Ticket, seu sistema para controlar entrada e saída dos carros do seu estacionamento!
                         </div>
                     </div>
-<script>
 
-$(function(){
-    $('.btn').on('click', function() {
-    var $this = $(this);
-  $this.button('loading');
-    setTimeout(function() {
-       $this.button('reset');
-   }, 8000);
-});
-
-});
-
-</script>
                     <form name="busca_carro" method="post" action="" class="inline">
                         <h3>Digite a placa do carro</h3>
                         <div class="row">
                             <div class="col-sm-8">
-                                <input type="text" name="placa" placeholder="XXX-YYYY" class="form-control">
+                                <input type="text" name="placa" id="placa-buscar" placeholder="XXX-YYYY" class="form-control">
                             </div>
                             <div class="col-sm-4">
-                                <button type="button" class="btn btn-info" id="load" data-loading-text="<i class='fa fa-spinner fa-spin'></i> Buscando">Buscar</button>
+                                <button type="button" class="btn btn-info" id="btn-buscar" data-loading-text="<i class='fa fa-spinner fa-spin'></i> Buscando">Buscar</button>
                             </div>
                         </div>
                     </form>
@@ -57,10 +44,10 @@ $(function(){
 
                         <div class="cadastrar-carro">
                             <p>Nenhum veículo encontrado</p>
-                            <input type="button" value="Cadastrar" class="btn btn-success">
+                            <input type="button" value="Cadastrar" id="btn-iniciar-cadastro" class="btn btn-success">
                         </div>
 
-                        <table class="table">
+                        <!-- <table class="table">
                             <tr>
                                 <th>Modelo</th>
                                 <th>Cor</th>
@@ -76,25 +63,115 @@ $(function(){
                                 <td>Vermelho</td>
                                 <td><input type="button" value="Saída" class="btn btn-warning"></td>
                             </tr>
-                        </table>
+                        </table> -->
                     </div>
+
+                    <script>
+
+                    $(function(){
+                        $('#btn-buscar').on('click', function() {
+                            var placa_buscar = $('#placa-buscar').val();
+
+                            if(placa_buscar){
+                                var $this = $(this);
+                                var resultado = $('.resultado');
+                                $this.button('loading');
+                                setTimeout(function() {
+                                    $this.button('reset');
+                                    resultado.slideDown();
+                                }, 100);
+                            }else{
+                                alert('Digite a placa do carro')
+                            }
+                        });
+                        $('#btn-iniciar-cadastro').on('click', function() {
+                            var resultado = $('.resultado');
+                            var form_cadastro = $('.form_cadastro');
+                            var placa_buscar = $('#placa-buscar').val();
+                            setTimeout(function() {
+                                resultado.html('');
+                                resultado.append(form_cadastro);
+                                $('#placa-cadastro').attr('value',placa_buscar)
+                                form_cadastro.fadeIn();
+                            }, 100);
+                        });
+                        $('#btn-cadastrar').on('click', function() {
+                            var placa_cadastrar = $('#placa-cadastrar').val();
+                            var modelo_cadastrar = $('#modelo-cadastrar').val();
+                            var cor_cadastrar = $('#cor-cadastrar').val();
+
+                            if((placa_cadastrar != '') && (modelo_cadastrar != '') && (cor_cadastrar != '')){
+
+                                var resultado = $('.resultado');
+                                var form_cadastrar = $('#form_cadastrar').serialize();
+                                var url = "estacionamento.php";
+
+                                $.post( url, form_cadastrar , function(){})
+                                .done(function(response) {
+                                    resultado.html(response);
+                                    resultado.slideDown();
+                                })
+                                .fail(function() {
+                                });
+
+                            }else{
+                                alert("Preencha todos os campos");
+                            }
+
+
+                        });
+                    });
+
+                    </script>
+
+                    <form action="" method="post" name="form_cadastrar" id="form_cadastrar">
+                        <h4>Cadastrar carro</h4>
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <div class="form-group">
+                                    <label for="placa">Placa</label>
+                                    <input type="text" name="placa" class="form-control" id="placa-cadastrar" placeholder="Placa" value="">
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="form-group">
+                                    <label for="modelo">Modelo</label>
+                                    <input type="text" name="modelo" class="form-control" id="modelo-cadastrar" placeholder="Modelo" value="">
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="form-group">
+                                    <label for="cor">Cor</label>
+                                    <input type="text" name="cor" class="form-control" id="cor-cadastrar" placeholder="Cor" value="">
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3">
+                                <label>&nbsp;</label><br>
+                                <input type="button" name="btn-cadastrar" value="Cadastrar" id="btn-cadastrar" class="btn btn-success">
+                            </div>
+                        </div>
+                    </form>
 
                 </div>
             </div>
             <div class="col-sm-6">
-                <div class="vagas">
-                    <?php
-                    for($i=0; $i<80; $i++){
-                        if($i % 8 == 0){
+                <div class="conteudo" style="height:100%;">
+                    <h3>Vagas Disponíveis</h3>
+                    <div class="vagas">
+                        <?php
+                        for($i=0; $i<80; $i++){
+                            if($i % 8 == 0){
+                                ?>
+                                <div class="clearfix"></div>
+                                <?php
+                            }
                             ?>
-                            <div class="clearfix"></div>
+                            <div class="vaga"></div>
                             <?php
                         }
                         ?>
-                        <div class="vaga"></div>
-                        <?php
-                    }
-                    ?>
+                    </div>
                 </div>
             </div>
         </div>
